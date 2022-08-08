34a09b56872e001baf9a4fb0bd53bafa7589545e
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-08-08 14:59:01 +0900

    places search

--- src/api/Map.js
+++ src/api/Map.js
@@ -1,6 +1,13 @@
+import React from 'react'
 import { useEffect, useMemo, useState } from "react";
-import axios from 'axios'
-import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import axios from 'axios';
+
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from "@react-google-maps/api";
+import usePlacesAutocomplete, {  getGeocode, getLatLng } from 'use-places-autocomplete';
+import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption } from '@reach/combobox';
+
+import "../assets/css/Map.css";
+import mapStyles from '../assets/css/mapStyles';
 
 // ホットペッパー　api
 const KEY = '6fca1552a5354f20';
@@ -29,6 +36,7 @@ const containerStyle = {
 function Home() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCOVIadCWc7aJkhQakeh_A38FRwwsxByvo",
+    libraries : ["places"],
   });
 
   if(!isLoaded) return <div>...Loading</div>;
@@ -37,6 +45,7 @@ function Home() {
 
 function Map() {
   const [shop, setShop] = useState();
+  const [selected, setSelected] = useState(null);
 
   const center = useMemo(() => ({lat:35.652832, lng:139.839478}), []);
 
@@ -50,19 +59,89 @@ function Map() {
   },[])
 
   return (
-    <GoogleMap
-      zoom={10}
-      center={center}
-      mapContainerStyle={containerStyle}
-    >
-      { shop &&
-        shop.map((item, index) => (
-          <Marker key={index} position={{lat:item.lat, lng:item.lng}} />
-        ))
-      }
-    </GoogleMap>
+    <div>
+
+      <Search />
+
+      <GoogleMap
+        zoom={10}
+        center={center}
+        mapContainerStyle={containerStyle}
+        onClick={(event) => {
+          console.log(event);
+        }}
+      >
+        { shop &&
+          shop.map((item, index) => (
+            <Marker
+              key={index} 
+              position={{lat:item.lat, lng:item.lng}} 
+              onClick={() => {
+                  setSelected(item);
+                  console.log(item);
+              }}
+          />
+          ))
+        }
+
+        {selected ? (
+          <InfoWindow 
+              position={{lat: selected.lat, lng: selected.lng}}
+              onCloseClick={() => {
+                  setSelected(null);
+              }}
+          >
+          <div>
+              <h2>{selected.access}</h2>
+              <p>address : selected.address</p>
+          </div>
+          </InfoWindow>):null
+        }
+      </GoogleMap>
+    </div>
   )
   
+}
+
+function Search() {
+  const {
+    ready, 
+    value, 
+    suggestions: {status, data}, 
+    setValue, 
+    clearSuggetions
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 35.652832, lng: () => 139.839478 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className='search'>
+      <Combobox onSelect={(address) => {
+        console.log(address);
+      }}
+      >
+        <ComboboxInput 
+          value={value} 
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter an address" 
+        />
+        <ComboboxPopover>
+          {status == "OK" && 
+            data.map(({id, description}) => (
+              <ComboboxOption key={id} value={description} />
+          ))}
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+    
+  );
+    
 }
 
 export default Home;
