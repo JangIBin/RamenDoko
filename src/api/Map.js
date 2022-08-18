964eaefc9d70bdfa7b01c2b2e123130f638d3571
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-08-19 08:31:24 +0900

    hosting github pages

--- src/api/Map.js
+++ src/api/Map.js
@@ -92,7 +92,7 @@ function Map() {
     });
 
     const hotpepper = axios.create ({
-      baseURL: 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1'
+      baseURL: '/hotpepper/gourmet/v1'
     })
 
     const fetchSearchData = async(mapRange, latLocate, lngLocate) =>{
