6468f4fa860936b7880389725db83ba33a322455
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-08-19 11:39:41 +0900

    hosting github pages test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'; 
 
 import Map from "./api/Map"
@@ -8,13 +8,14 @@ import Start from './components/Start';
 
 function App() {
   return (
-
-    <Routes>
-      <Route path='/' element={<Start />} />
-      <Route path='/map' element={<Map />} />
-      <Route path='/detail' element={<Detail />} />
-      
-    </Routes>
+    <BrowserRouter basename='/RamenDoko'>
+      <Routes>
+          <Route path='/' element={<Start />} exact />
+          <Route path='/map' element={<Map />} />
+          <Route path='/detail' element={<Detail />} />
+       </Routes>
+    </BrowserRouter>
+    
   );
 }
 
