3e2ed864f5d8eebe8708e26b90ec806aa924d75a
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-08-19 09:29:53 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/RamenDoko/precache-manifest.d8478715218d4c316fed876a9466284d.js"
+  "/RamenDoko/precache-manifest.08d32e7774f15bdd2aa076708c468790.js"
 );
 
 workbox.clientsClaim();
